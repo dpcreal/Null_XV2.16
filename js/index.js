c6c90b092bfb52647889065dc1cc87d40b5f40d7
Glaeesas <Sparefornow2026@outlook.com>
2026-03-25 08:43:49 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -800,10 +800,12 @@ function setPreset(object) {
 function updatePreset() {
     const selected = document.getElementById('presets').value;
     const preset = presets[selected];
-    if (!preset) {
-        showNotification("Please Wait Thirty Minutes, Because This Cloak Option Has No Javascript Code Tied To It");
+    
+    if (!preset || !preset.url || !preset.title || !preset.icon) { //This sees if there is any javascript code for the cloak selected and if there isn't gives you a notification
+        showNotification("This cloak option does not currently have any JavaScript functionality associated with it. Please wait until it has been fully implemented.");
         return;
     }
+
     setPreset(preset);
 }
 if (preferences.cloak && window.location.href == window.top.location.href) {
